3438439240103c51ff47f47490effd8ad5c4f7d7
Gareth Marshall <garethm@acm.org>
2010-11-08 23:45:01 +1000

    I didn't realise at first that splice actually modified daysOfWeek. I have added a test to make this clear

--- topics/about_arrays.js
+++ topics/about_arrays.js
@@ -9,30 +9,31 @@ $(document).ready(function(){
 		equals(favouriteThings[1], __, 'what is in the second position of the array?');
 		equals(favouriteThings[2], __, 'what is in the third position of the array?');
 	});
-	
+
 	test("array type", function() {
 		equals(typeof([]), __, 'what is the type of an array?');
 	});
-	
+
 	test("length", function() {
 		var collection = ['a','b','c'];
 		equals(collection.length, __, 'what is the length of the collection array?');
 	});
-	
+
 	test("delete", function() {
 		var daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 		var workingWeek = daysOfWeek.splice(__, __);
 		ok(workingWeek.equalTo([__]), 'what is the value of workingWeek?');
+    ok(daysOfWeek.equalTo([__]), 'what is the value of daysOfWeek?');
 	});
-	
+
 	test("stack methods", function() {
 		var stack = [];
 		stack.push("first");
 		stack.push("second");
-		
+
 		equals(stack.pop(), __, 'what will be the first value poped off the stack?');
 		equals(stack.pop(), __, 'what will be the second value poped off the stack?');
 	});
-	
+
 });
 
