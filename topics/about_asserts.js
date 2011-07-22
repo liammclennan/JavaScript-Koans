5a763bbc6736e79131d523dced08777796d205a3
Liam McLennan <liam.mclennan@gmail.com>
2011-07-23 06:25:38 +1000

    removing jquery wrapper and normalizing indentation

--- topics/about_asserts.js
+++ topics/about_asserts.js
@@ -1,18 +1,14 @@
 
-$(document).ready(function(){
-	
-	module("About Asserts (topics/about_asserts.js)");
+module("About Asserts (topics/about_asserts.js)");
 
-	test("ok", function() {
-		ok(false, 'what will satisfy the ok assertion?');
-	});
-	
-	test("not", function() {
-		not(__, 'what is a false value?');
-	});
-	
-	test("equals", function() {
-		equals(1+1, __, 'what will satisfy the equals assertion?');
-	});
-	
+test("ok", function() {
+    ok(false, 'what will satisfy the ok assertion?');
+});
+
+test("not", function() {
+    not(__, 'what is a false value?');
+});
+
+test("equals", function() {
+    equals(1+1, __, 'what will satisfy the equals assertion?');
 });
--- topics/about_operators.js
+++ topics/about_operators.js
@@ -1,50 +1,47 @@
-$(document).ready(function(){
 
-	module("About Operators (topics/about_operators.js)");
+module("About Operators (topics/about_operators.js)");
 
-  test("addition", function() {
-    var result = 0;
-    //starting i at 0, add i to result and increment i by 1 until i is equal to 5
-    for (var i = 0; i <= 5; i++) {
-      result = result + i;
-    }
-    equals(result, __, "What is the value of result?");
-  });
+test("addition", function() {
+  var result = 0;
+  //starting i at 0, add i to result and increment i by 1 until i is equal to 5
+  for (var i = 0; i <= 5; i++) {
+    result = result + i;
+  }
+  equals(result, __, "What is the value of result?");
+});
 
-  test("assignment addition", function() {
-    var result = 0;
-    for (var i = 0; i <=5; i++) {
-      //the code below is just like saying result = result + i; but is more concise
-      result += i;
-    }
-    equals(result, __, "What is the value of result?");
-  });
+test("assignment addition", function() {
+  var result = 0;
+  for (var i = 0; i <=5; i++) {
+    //the code below is just like saying result = result + i; but is more concise
+    result += i;
+  }
+  equals(result, __, "What is the value of result?");
+});
 
-  test("subtraction", function() {
-    var result = 5;
-    for (var i = 0; i <= 2; i++) {
-      result = result - i;
-    }
-    equals(result, __, "What is the value of result?");
-  });
+test("subtraction", function() {
+  var result = 5;
+  for (var i = 0; i <= 2; i++) {
+    result = result - i;
+  }
+  equals(result, __, "What is the value of result?");
+});
 
-  test("assignment subtraction", function() {
-    var result = 5;
-    for (var i = 0; i <= 2; i++) {
-      result -= i;
-    }
-    equals(result, __, "What is the value of result?");
-  });
+test("assignment subtraction", function() {
+  var result = 5;
+  for (var i = 0; i <= 2; i++) {
+    result -= i;
+  }
+  equals(result, __, "What is the value of result?");
+});
 
-  //Assignment operators are available for multiplication and division as well
-  //let's do one more, the modulo operator, used for showing division remainder
+//Assignment operators are available for multiplication and division as well
+//let's do one more, the modulo operator, used for showing division remainder
 
-  test("modulus", function() {
-    var result = 10;
-    var x = 5;
-    //again this is exactly the same as result = result % x
-    result %= x;
-    equals(result, __, "What is the value of result?");
-  });
-
+test("modulus", function() {
+  var result = 10;
+  var x = 5;
+  //again this is exactly the same as result = result % x
+  result %= x;
+  equals(result, __, "What is the value of result?");
 });
