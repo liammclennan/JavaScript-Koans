7d149470d5377e108473a0700168819ecee81109
Liam McLennan <liam.mclennan@gmail.com>
2011-07-23 07:00:30 +1000

    fixing indentation, removing jquery wrappers

--- topics/about_regular_expressions.js
+++ topics/about_regular_expressions.js
@@ -1,35 +1,31 @@
 
-$(document).ready(function(){
+module("About Regular Expressions (topics/about_regular_expressions.js)");
 
-	module("About Regular Expressions (topics/about_regular_expressions.js)");
-	
-	test("exec", function() {
-		var numberFinder = /(\d).*(\d)/;
-		var results = numberFinder.exec("what if 6 turned out to be 9?");
-		ok(results.equalTo([__, __, __]), 'what is the value of results?');		
-	});
-	
-	test("test", function() {
-		var containsSelect = /select/.test("  select * from users ");
-		equals(containsSelect, __, 'does the string provided contain "select"?');
-	});
-	
-	test("match", function() {
-		var matches = "what if 6 turned out to be 9?".match(/(\d)/g);
-		ok(matches.equalTo([__, __]), 'what is the value of matches?');
-	});
-	
-	test("replace", function() {
-		var pie = "apple pie".replace("apple", "strawberry");
-		equals(pie, __, 'what is the value of pie?');
-		
-		pie = "what if 6 turned out to be 9?".replace(/\d/g, function(number) { // the second parameter can be a string or a function
-			var map = {'6': 'six','9': 'nine'};
-			return map[number];
-		});
-		equals(pie, __, 'what is the value of pie?');
-	});
-	
-	// THE END
-	
+test("exec", function() {
+    var numberFinder = /(\d).*(\d)/;
+    var results = numberFinder.exec("what if 6 turned out to be 9?");
+    ok(results.equalTo([__, __, __]), 'what is the value of results?');		
 });
+
+test("test", function() {
+    var containsSelect = /select/.test("  select * from users ");
+    equals(containsSelect, __, 'does the string provided contain "select"?');
+});
+
+test("match", function() {
+    var matches = "what if 6 turned out to be 9?".match(/(\d)/g);
+    ok(matches.equalTo([__, __]), 'what is the value of matches?');
+});
+
+test("replace", function() {
+    var pie = "apple pie".replace("apple", "strawberry");
+    equals(pie, __, 'what is the value of pie?');
+
+    pie = "what if 6 turned out to be 9?".replace(/\d/g, function(number) { // the second parameter can be a string or a function
+        var map = {'6': 'six','9': 'nine'};
+        return map[number];
+    });
+    equals(pie, __, 'what is the value of pie?');
+});
+
+// THE END
--- topics/about_scope.js
+++ topics/about_scope.js
@@ -1,34 +1,28 @@
 
-$(document).ready(function(){
+module("About Scope (topics/about_scope.js)");
 
-	module("About Scope (topics/about_scope.js)");
+thisIsAGlobalVariable = 77;
 
-	thisIsAGlobalVariable = 77;
-	
-	test("global variables", function() {
-		equals(thisIsAGlobalVariable, __, 'is thisIsAGlobalVariable defined in this scope?');
-	});
-	
-	test("variables declared inside of a function", function() {
-		var outerVariable = "outer";
-
-        // this is a self-invoking function. Notice that it calls itself at the end ().
-		(function() {
-			var innerVariable = "inner";
-			equals(outerVariable, __, 'is outerVariable defined in this scope?');
-			equals(innerVariable, __, 'is innerVariable defined in this scope?');
-		})();
-		
-		equals(outerVariable, __, 'is outerVariable defined in this scope?');
-		var isInnerVariableDefined = true;
-		try {
-			innerVariable
-		} catch(e) {
-			isInnerVariableDefined = false;
-		}
-		equals(isInnerVariableDefined, __, 'is innerVariable defined in this scope?');
-	});
-		
-
+test("global variables", function() {
+    equals(thisIsAGlobalVariable, __, 'is thisIsAGlobalVariable defined in this scope?');
 });
-	
+
+test("variables declared inside of a function", function() {
+    var outerVariable = "outer";
+
+    // this is a self-invoking function. Notice that it calls itself at the end ().
+    (function() {
+        var innerVariable = "inner";
+        equals(outerVariable, __, 'is outerVariable defined in this scope?');
+        equals(innerVariable, __, 'is innerVariable defined in this scope?');
+    })();
+
+    equals(outerVariable, __, 'is outerVariable defined in this scope?');
+    var isInnerVariableDefined = true;
+    try {
+        innerVariable
+    } catch(e) {
+        isInnerVariableDefined = false;
+    }
+    equals(isInnerVariableDefined, __, 'is innerVariable defined in this scope?');
+});
