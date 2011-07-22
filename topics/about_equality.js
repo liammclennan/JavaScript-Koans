a9cbecbdffcf487089180bc63b81fe9881811b95
Liam McLennan <liam.mclennan@gmail.com>
2011-07-23 06:27:22 +1000

    cont.

--- topics/about_equality.js
+++ topics/about_equality.js
@@ -1,27 +1,22 @@
 
-$(document).ready(function(){
+module("About Equality (topics/about_equality.js)");
 
-	module("About Equality (topics/about_equality.js)");
-
-	test("numeric equality", function() {
-		equals(3 + __,  7, 'hmmmm?');
-	});
-
-	test("string equality", function() {
-		equals("3" + __, "37", "concatenate the strings");
-	});
-
-	test("equality without type coercion", function() {
-		ok(3 === __, 'what is exactly equal to 3?');
-	});
-
-	test("equality with type coercion", function() {
-		ok(3 == "__", 'what string is equal to 3, with type coercion?');
-	});
-
-	test("string literals", function() {
-		equals("frankenstein", '__', "quote types are interchangable, but must match.");
-	});
-
+test("numeric equality", function() {
+    equals(3 + __,  7, 'hmmmm?');
 });
 
+test("string equality", function() {
+    equals("3" + __, "37", "concatenate the strings");
+});
+
+test("equality without type coercion", function() {
+    ok(3 === __, 'what is exactly equal to 3?');
+});
+
+test("equality with type coercion", function() {
+    ok(3 == "__", 'what string is equal to 3, with type coercion?');
+});
+
+test("string literals", function() {
+    equals("frankenstein", '__', "quote types are interchangable, but must match.");
+});
