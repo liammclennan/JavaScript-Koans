09ea0b149a4db453882da8c30f495a34f5938c0e
Liam McLennan <liam.mclennan@gmail.com>
2011-07-23 06:30:32 +1000

    cont.

--- topics/about_assignment.js
+++ topics/about_assignment.js
@@ -1,16 +1,12 @@
 
-$(document).ready(function(){
+module("About Assignment (topics/about_assignment.js)");
 
-	module("About Assignment (topics/about_assignment.js)");
+test("local variables", function() {
+    var temp = __;
+    equals(1, temp, "Assign a value to the variable temp");
+});
 
-	test("local variables", function() {
-		var temp = __;
-		equals(1, temp, "Assign a value to the variable temp");
-	});
-
-	test("global variables", function() {
-		temp = 1;
-		equals(temp, window.__, 'global variables are assigned to the window object');
-	});
-
+test("global variables", function() {
+    temp = 1;
+    equals(temp, window.__, 'global variables are assigned to the window object');
 });
--- topics/about_truthyness.js
+++ topics/about_truthyness.js
@@ -1,26 +1,22 @@
 
-$(document).ready(function(){
+module("About Truthyness (topics/about_truthyness.js)");
 
-	module("About Truthyness (topics/about_truthyness.js)");
+test("truthyness of positive numbers", function() {
+    var oneIsTruthy = 1 ? true : false;
+    equals(oneIsTruthy, __, 'is one truthy?');
+});
 
-	test("truthyness of positive numbers", function() {
-		var oneIsTruthy = 1 ? true : false;
-		equals(oneIsTruthy, __, 'is one truthy?');
-	});
-	
-	test("truthyness of negative numbers", function() {
-		var negativeOneIsTruthy = -1 ? true : false;
-		equals(negativeOneIsTruthy, __, 'is -1 truthy?');
-	});
-	
-	test("truthyness of zero", function() {
-		var zeroIsTruthy = 0 ? true : false;
-		equals(zeroIsTruthy, __, 'is 0 truthy?');
-	});
+test("truthyness of negative numbers", function() {
+    var negativeOneIsTruthy = -1 ? true : false;
+    equals(negativeOneIsTruthy, __, 'is -1 truthy?');
+});
 
-	test("truthyness of null", function() {
-		var nullIsTruthy = null ? true : false;
-		equals(nullIsTruthy, __, 'is null truthy?');
-	});
-	
+test("truthyness of zero", function() {
+    var zeroIsTruthy = 0 ? true : false;
+    equals(zeroIsTruthy, __, 'is 0 truthy?');
+});
+
+test("truthyness of null", function() {
+    var nullIsTruthy = null ? true : false;
+    equals(nullIsTruthy, __, 'is null truthy?');
 });
