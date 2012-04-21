0cb75f1e2b4a978b214f084639fab462bd3ebe3b
Liam McLennan <liam.mclennan@gmail.com>
2012-04-21 22:10:19 +1000

    added 'about this'

---
+++ topics/about_this.js
@@ -0,0 +1,44 @@
+module("About this (topics/about_this.js)");
+
+test("'this' inside a method", function () {
+	var person = {
+		name: 'bob',
+		intro: function () {
+			return "Hello, my name is " + this.__;
+		} 
+	}
+	equals(person.intro(), "Hello, my name is bob");
+});
+
+test("'this' on unattached function", function () {
+	var person = {
+		handle: 'bob',
+		intro: function () {
+			return "Hello, my name is " + this.handle;
+		} 
+	}
+
+	var alias = person.intro;
+	
+	// if the function called as an object property 'this' is the global context 
+	// (window in a browser)
+	window.__ = 'Peter';
+	equals("Hello, my name is Peter", alias());
+});
+
+test("'this' set explicitly", function () {
+	var person = {
+		handle: 'bob',
+		intro: function () {
+			return "Hello, my name is " + this.handle;
+		} 
+	}
+
+	// calling a function with 'call' lets us assign 'this' explicitly
+	var message = person.intro.call({__: "Frank"});
+	equals(message, "Hello, my name is Frank");
+});
+
+// extra credit: underscore.js has a 'bind' function http://documentcloud.github.com/underscore/#bind
+// read the source and see how it is implemented
+
