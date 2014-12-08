0ddb4519cfc132682c0cd3e28e368fe67058a17c
Richard Littauer <richard.littauer@gmail.com>
2014-12-08 14:35:17 -0800

    Grammar fixes

--- topics/about_functions_and_closure.js
+++ topics/about_functions_and_closure.js
@@ -3,7 +3,7 @@ module("About Functions And Closure (topics/about_functions_and_closure.js)");
 test("defining functions directly", function() {
     var result = "a";
     function changeResult() {
-        // the ability to access a variables defined in the same scope as the function is known as 'closure'
+        // the ability to access variables defined in the same scope as the function is known as 'closure'
         result = "b";
     }
     changeResult();
@@ -54,7 +54,8 @@ test("using call to invoke function",function(){
     //another way to invoke a function is to use the call function which allows 
     //you to set the callers "this" context.  Call can take any number of arguments: 
     //the first one is always the context that this should be set to in the called
-    //function, and the arguments to be sent to the function,multiple arguments are separated by commas.
+    //function, and the rest are the arguments to be sent to the function. 
+    //Multiple arguments are separated by commas.
     var result = invokee.call("I am this!", "Where did it come from?");
         
     equal(__, result, "what will the value of invokee's this be?");
@@ -67,7 +68,8 @@ test("using apply to invoke function",function(){
     
     //similar to the call function is the apply function.  Apply only has two
     //arguments:  the first is the context that this should be set to in the called
-    //function and and array of arguments to be passed into the called function.
+    //function and the second is an array of arguments to be passed into the called 
+    //function.
     var result = invokee.apply("I am this!", ["I am arg1","I am arg2"]);
         
     equal(__, result, "what will the value of invokee's this be?");
