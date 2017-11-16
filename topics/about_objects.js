f9cc5b5f681a28e3e280132f417ec81ac81a4b6c
ginkomix <accer96226@gmail.com>
2017-11-16 22:33:51 +0300

    done

--- topics/about_objects.js
+++ topics/about_objects.js
@@ -3,13 +3,13 @@ module("About Objects (topics/about_objects.js)");
 
 test("object type", function() {
     var empty_object = {};
-    equal(__, typeof(empty_object), 'what is the type of an object?');
+    equal('object', typeof(empty_object), 'what is the type of an object?');
 });
 
 test("object literal notation", function() {
     var person = {
-        __:__,
-        __:__
+        name:"Amory Blaine",
+        age:102
     };
     equal("Amory Blaine", person.name, "what is the person's name?");
     equal(102, person.age, "what is the person's age?");
@@ -17,16 +17,16 @@ test("object literal notation", function() {
 
 test("dynamically adding properties", function() {
     var person = {};
-    person.__ = "Amory Blaine";
-    person.__ = 102;
+    person.name = "Amory Blaine";
+    person.age = 102;
     equal("Amory Blaine", person.name, "what is the person's name?");
     equal(102, person.age, "what is the person's age?");
 }); 
 
 test("adding properties from strings", function() {
     var person = {};
-    person["__"] = "Amory Blaine";
-    person["__"] = 102;
+    person["name"] = "Amory Blaine";
+    person["age"] = 102;
     equal("Amory Blaine", person.name, "what is the person's name?");
     equal(102, person.age, "what is the person's age?");
 });
@@ -36,7 +36,7 @@ test("adding functions", function() {
         name: "Amory Blaine",
         age: 102,
         toString: function() {
-            return __;  // HINT: use the 'this' keyword to refer to the person object.
+            return 'I '+this.name+' am '+this.age+' years old.';  // HINT: use the 'this' keyword to refer to the person object.
         }
     };
     equal("I Amory Blaine am 102 years old.", person.toString(), "what should the toString function be?");
