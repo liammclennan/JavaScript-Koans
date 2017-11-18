5249359cc609c560503506ac940d493f910246c9
Andrew1931 <andrewk1931@gmail.com>
2017-11-18 18:02:29 +0300

    Koans done

--- topics/about_regular_expressions.js
+++ topics/about_regular_expressions.js
@@ -14,18 +14,18 @@ test("test", function() {
 
 test("match", function() {
     var matches = "what if 6 turned out to be 9?".match(/(\d)/g);
-    ok(matches.equalTo([__, __]), 'what is the value of matches?');
+    ok(matches.equalTo([6, 9]), 'what is the value of matches?');
 });
 
 test("replace", function() {
     var pie = "apple pie".replace("apple", "strawberry");
-    equal(__, pie, 'what is the value of pie?');
+    equal(strawberry, pie, 'what is the value of pie?');
 
     pie = "what if 6 turned out to be 9?".replace(/\d/g, function(number) { // the second parameter can be a string or a function
         var map = {'6': 'six','9': 'nine'};
         return map[number];
     });
-    equal(__, pie, 'what is the value of pie?');
+    equal("what if six turned out to be nine?", pie, 'what is the value of pie?');
 });
 
 // THE END
