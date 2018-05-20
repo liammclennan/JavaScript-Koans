41db8e490f5fb42d307119c8a4c656781df77749
Aleksandr Tikhonovich <39463222+aleksandrtikh@users.noreply.github.com>
2018-05-20 19:09:42 +0300

    Solved all koans.

--- topics/about_asserts.js
+++ topics/about_asserts.js
@@ -2,13 +2,13 @@
 module("About Asserts (topics/about_asserts.js)");
 
 test("ok", function() {
-    ok(__ === true, 'what will satisfy the ok assertion?');
+    ok(true === true, 'what will satisfy the ok assertion?');
 });
 
 test("not ok", function() {
-    ok(__ === false, 'what is a false value?');
+    ok(false === false, 'what is a false value?');
 });
 
 test("equal", function() {
-    equal(__, 1 + 1, 'what will satisfy the equal assertion?');
+    equal(2, 1 + 1, 'what will satisfy the equal assertion?');
 });
--- topics/about_this.js
+++ topics/about_this.js
@@ -4,7 +4,7 @@ test("'this' inside a method", function () {
 	var person = {
 		name: 'bob',
 		intro: function () {
-			return "Hello, my name is " + this.__;
+			return "Hello, my name is " + this.name;
 		} 
 	}
 	equal(person.intro(), "Hello, my name is bob", "If an object has a method can you access properties inside it?");
@@ -22,7 +22,7 @@ test("'this' on unattached function", function () {
 	
 	// if the function is not called as an object property 'this' is the global context 
 	// (window in a browser). This is an example. Please do not do this in practise.
-	window.__ = 'Peter';
+	window.globalName = 'Peter';
 	equal(alias(), "Hello, my name is Peter", "What does 'this' refer to when it is not part of an object?");
 });
 
@@ -35,7 +35,7 @@ test("'this' set explicitly", function () {
 	}
 
 	// calling a function with 'call' lets us assign 'this' explicitly
-	var message = person.intro.call({__: "Frank"});
+	var message = person.intro.call({name: "Frank"});
 	equal(message, "Hello, my name is Frank", "What does 'this' refer to when you use the 'call()' method?");
 });
 
