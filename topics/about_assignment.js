d024c6eb7e08d678b6fdf9accfa861a4dac1dd8a
FreibergVlad <vladfreiberg97@gmail.com>
2018-05-20 17:33:22 +0300

    add solution

--- topics/about_assignment.js
+++ topics/about_assignment.js
@@ -1,12 +1,12 @@
-
+var temp = 1;
 module("About Assignment (topics/about_assignment.js)");
 
 test("local variables", function() {
-    var temp = __;
+    var temp = 1;
     equal(temp, 1, "Assign a value to the variable temp");
 });
 
 test("global variables", function() {
-    temp = 1; // Not using var is an example. Always use var in practise.
-    equal(window.__, temp, 'global variables are assigned to the window object');
+    // Not using var is an example. Always use var in practise.
+    equal(window.temp, temp, 'global variables are assigned to the window object');
 });
