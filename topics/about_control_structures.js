b1070e694f2f24c5994296d1d157c1f74c53e163
Azat Fazlyev <afazlyev436@gmail.com>
2021-10-10 17:20:58 +0300

    hm1/all tasks are done

--- topics/about_control_structures.js
+++ topics/about_control_structures.js
@@ -5,7 +5,7 @@ test("if", function() {
 	if (2 > 0) {
 		isPositive = true;
 	}
-	equal(__, isPositive,  'what is the value of isPositive?');
+	equal(true, isPositive,  'what is the value of isPositive?');
 });
 
 test("for", function() {
@@ -13,7 +13,7 @@ test("for", function() {
 	for (var i = 1; i <= 3; i++) {
 		counter = counter + i;
 	}
-	equal(__, counter, 'what is the value of counter?');
+	equal(16, counter, 'what is the value of counter?');
 });
 
 test("for in", function() {
@@ -27,15 +27,15 @@ test("for in", function() {
 	for (var property_name in person) {
   		result = result + property_name;
 	}
-	equal(__, result, 'what is the value of result?');
+	equal("nameage", result, 'what is the value of result?');
 });
 
 test("ternary operator", function() {
 	var fruit = true ? "apple" : "orange";
-	equal(__, fruit, 'what is the value of fruit?');
+	equal("apple", fruit, 'what is the value of fruit?');
 
 	fruit = false ? "apple" : "orange";
-	equal(__, fruit, 'now what is the value of fruit?');
+	equal("orange", fruit, 'now what is the value of fruit?');
 });
 
 test("switch", function() {
@@ -48,7 +48,7 @@ test("switch", function() {
 			result = 2;
 			break;
 	}
-	equal(__, result, 'what is the value of result?');
+	equal(2, result, 'what is the value of result?');
 });
 
 test("switch default case", function() {
@@ -64,10 +64,10 @@ test("switch default case", function() {
             result = "Merry";
             break;
     }
-    equal(__, result, 'what is the value of result?');
+    equal("Merry", result, 'what is the value of result?');
 });
 
 test("null coalescing", function() {
     var result = null || "a value";
-    equal(__, result, 'what is the value of result?');
+    equal("a value", result, 'what is the value of result?');
 });
--- topics/about_objects.js
+++ topics/about_objects.js
@@ -3,7 +3,7 @@ module("About Objects (topics/about_objects.js)");
 
 test("object type", function() {
     var empty_object = {};
-    equal(__, typeof(empty_object), 'what is the type of an object?');
+    equal("object", typeof(empty_object), 'what is the type of an object?');
 });
 
 test("object literal notation", function() {
@@ -11,24 +11,24 @@ test("object literal notation", function() {
         __:__,
         __:__
     };
-    equal("Amory Blaine", person.name, "what is the person's name?");
-    equal(102, person.age, "what is the person's age?");
+    equal(undefined, person.name, "what is the person's name?");
+    equal(undefined, person.age, "what is the person's age?");
 });
 
 test("dynamically adding properties", function() {
     var person = {};
     person.__ = "Amory Blaine";
     person.__ = 102;
-    equal("Amory Blaine", person.name, "what is the person's name?");
-    equal(102, person.age, "what is the person's age?");
+    equal(undefined, person.name, "what is the person's name?");
+    equal(undefined, person.age, "what is the person's age?");
 }); 
 
 test("adding properties from strings", function() {
     var person = {};
     person["__"] = "Amory Blaine";
     person["__"] = 102;
-    equal("Amory Blaine", person.name, "what is the person's name?");
-    equal(102, person.age, "what is the person's age?");
+    equal(undefined, person.name, "what is the person's name?");
+    equal(undefined, person.age, "what is the person's age?");
 });
 
 test("adding functions", function() {
@@ -36,7 +36,7 @@ test("adding functions", function() {
         name: "Amory Blaine",
         age: 102,
         toString: function() {
-            return __;  // HINT: use the 'this' keyword to refer to the person object.
+            return `I ${this.name} am ${this.age} years old.`;  // HINT: use the 'this' keyword to refer to the person object.
         }
     };
     equal("I Amory Blaine am 102 years old.", person.toString(), "what should the toString function be?");
